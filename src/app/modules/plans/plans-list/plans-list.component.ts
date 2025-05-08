cd9ab42e58b9594f0d194196c6d631c98defe84d
Kuriakose George <kuriakose00@gmail.com>
2025-05-08 17:02:00 +0530

    cuddle plan list, side menu, header

--- src/app/modules/plans/plans-list/plans-list.component.ts
+++ src/app/modules/plans/plans-list/plans-list.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-plans-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './plans-list.component.html',
   styleUrl: './plans-list.component.css'
 })
 export class PlansListComponent {
+
+  plans: any[] = [
+    {
+      name: 'Basic Plan',
+      description: 'Description about basic plan',
+      amount: 1999,
+      validity: '30 Days',
+      bookings: 3
+    },
+    {
+      name: 'Standard Plan',
+      description: 'Description about standard plan',
+      amount: 3999,
+      validity: '60 Days',
+      bookings: 5
+    },
+    {
+      name: 'Premium Plan',
+      description: 'Description about premium plan',
+      amount: 5999,
+      validity: '90 Days',
+      bookings: 8
+    }
+  ];
 
 }
