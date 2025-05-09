7dc0415a85ba5db8d02ffed10a0fa598863c9a06
Kuriakose George <kuriakose00@gmail.com>
2025-05-09 17:05:07 +0530

    side menu responsive

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { SidemenuComponent } from "../../shared/components/sidemenu/sidemenu.component";
 import { RouterOutlet } from '@angular/router';
@@ -6,11 +6,35 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, SidemenuComponent,RouterOutlet,CommonModule],
+  imports: [HeaderComponent, SidemenuComponent, RouterOutlet, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  showSidebar:boolean = false;
 
+  toggleSidebar() {
+    this.showSidebar = !this.showSidebar;
+  }
+
+  isLargeScreen(): boolean {
+    return window.innerWidth >= 992;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (this.isLargeScreen()) {
+      this.showSidebar = true;
+    } else {
+      this.showSidebar = false;
+    }
+  }
+
+  ngOnInit() {
+    if (this.isLargeScreen()) {
+      this.showSidebar = true;
+    } else {
+      this.showSidebar = false;
+    }
+  }
 }
-  
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+@Output() toggle = new EventEmitter<void>();
 
-}
+onToggleSidebar() {
+  this.toggle.emit();
+}}
