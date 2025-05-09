dca521b67e89fa405f0b8ded6e5f9939d5f1e6d5
Kuriakose George <kuriakose00@gmail.com>
2025-05-09 10:17:34 +0530

    cuddle plan list, side menu, header

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { SidemenuComponent } from "../../shared/components/sidemenu/sidemenu.component";
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, SidemenuComponent,RouterOutlet],
+  imports: [HeaderComponent, SidemenuComponent,RouterOutlet,CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
