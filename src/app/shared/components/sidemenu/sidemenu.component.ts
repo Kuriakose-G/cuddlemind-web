1145ed0156433e9182a3c92b0ecdb7ade49f17f5
Kuriakose George <kuriakose00@gmail.com>
2025-05-16 12:10:16 +0530

    routing set

--- src/app/shared/components/sidemenu/sidemenu.component.ts
+++ src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
 export class SidemenuComponent {
+
+  constructor(private router: Router) { }
+
+  navigate(path: string) {
+    this.router.navigate([path]);
+  }
 
 }
