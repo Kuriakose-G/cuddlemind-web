cba8772279f49aa97652e0b464140340b33e9111
Kuriakose George <kuriakose00@gmail.com>
2025-05-20 15:57:13 +0530

    plan, user edit page added

--- src/app/modules/all-list/all-list/all-list.component.ts
+++ src/app/modules/all-list/all-list/all-list.component.ts
@@ -1,34 +1,85 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AlertService } from '../../../shared/components/alert/service/alert.service';
 
 @Component({
   selector: 'app-all-list',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './all-list.component.html',
   styleUrl: './all-list.component.css'
 })
 export class AllListComponent {
-  plans: any[] = [
+  userForm: FormGroup;
+
+  users: any[] = [
     {
       name: 'Basic Plan',
-      description: 'Description about basic plan',
+      email: 'Description about basic plan',
       amount: 1999,
       validity: '30 Days',
       bookings: 3
     },
     {
       name: 'Standard Plan',
-      description: 'Description about standard plan',
+      email: 'Description about standard plan',
       amount: 3999,
       validity: '60 Days',
       bookings: 5
     },
     {
       name: 'Premium Plan',
-      description: 'Description about premium plan',
+      email: 'Description about premium plan',
       amount: 5999,
       validity: '90 Days',
       bookings: 8
     }
   ];
+
+  constructor(
+    private modalService: NgbModal,
+    private fb: FormBuilder,
+    private alertService: AlertService
+  ) {
+    this.userForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3)]],
+      role: ['', Validators.required],
+      phone: ['', [Validators.pattern(/^\d{10}$/)]],
+      email: ['', [Validators.required, Validators.email]]
+    });
+
+  }
+
+  openModal(content: any) {
+    const buttonElement = document.activeElement as HTMLElement
+    buttonElement.blur();
+
+    this.modalService.open(content)
+  }
+
+  onSave(modal: any) {
+    if (this.userForm.valid) {
+      console.log('Form Value:', this.userForm.value);
+      const newPlan = this.userForm.value;
+      this.users.push(newPlan);
+
+      this.alertService.showAlert({
+        message: 'Plan Created',
+        type: 'success',
+        autoDismiss: true,
+        duration: 4000
+      })
+
+      this.userForm.reset();
+      modal.close('Save click');
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
+
+  close() {
+    this.userForm.reset()
+    this.modalService.dismissAll()
+  }
 }
--- src/app/modules/plans/plans-list/plans-list.component.ts
+++ src/app/modules/plans/plans-list/plans-list.component.ts
@@ -1,36 +1,99 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AlertService } from '../../../shared/components/alert/service/alert.service';
 
 @Component({
   selector: 'app-plans-list',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './plans-list.component.html',
   styleUrl: './plans-list.component.css'
 })
 export class PlansListComponent {
-
+  planForm: FormGroup;
   plans: any[] = [
     {
-      name: 'Basic Plan',
+      planName: 'Basic Plan',
       description: 'Description about basic plan',
       amount: 1999,
-      validity: '30 Days',
-      bookings: 3
+      validity: '30',
+      bookings: 3,
     },
     {
-      name: 'Standard Plan',
+      planName: 'Standard Plan',
       description: 'Description about standard plan',
       amount: 3999,
-      validity: '60 Days',
-      bookings: 5
+      validity: '60',
+      bookings: 5,
     },
     {
-      name: 'Premium Plan',
+      planName: 'Premium Plan',
       description: 'Description about premium plan',
       amount: 5999,
-      validity: '90 Days',
-      bookings: 8
+      validity: '90',
+      bookings: 8,
     }
   ];
+
+  constructor(
+    private modalService: NgbModal,
+    private fb: FormBuilder,
+    private alertService: AlertService
+  ) {
+    this.planForm = this.fb.group({
+      planName: ['', [Validators.required, Validators.minLength(3)]],
+      amount: ['', [Validators.required, Validators.min(1)]],
+      validity: ['', [Validators.required, this.positiveIntegerValidator]],
+      bookings: ['', [Validators.required, this.positiveIntegerValidator]],
+      description: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  positiveIntegerValidator(control: AbstractControl): ValidationErrors | null {
+    const val = control.value;
+    if (val === null || val === '' || val === undefined) {
+      return null; // Let 'required' handle empty values
+    }
+
+    const numberVal = +val;
+    if (isNaN(numberVal) || numberVal <= 0 || !Number.isInteger(numberVal)) {
+      return { positiveInteger: true };
+    }
+
+    return null;
+  }
+
+  openModal(content: any) {
+    const buttonElement = document.activeElement as HTMLElement
+    buttonElement.blur();
+
+    this.modalService.open(content)
+  }
+
+  onSave(modal: any) {
+    if (this.planForm.valid) {
+      console.log('Form Value:', this.planForm.value);
+      const newPlan = this.planForm.value;
+      this.plans.push(newPlan);
+
+      this.alertService.showAlert({
+        message: 'Plan Created',
+        type: 'success',
+        autoDismiss: true,
+        duration: 4000
+      })
+
+      this.planForm.reset();
+      modal.close('Save click');
+    } else {
+      this.planForm.markAllAsTouched();
+    }
+  }
+
+  close() {
+    this.planForm.reset()
+    this.modalService.dismissAll()
+  }
 
 }
